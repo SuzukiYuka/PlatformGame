72dbbd4652ed172e578bb54b68a6433d81ea209d
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-10-03 18:07:02 +0900

    play -> playOneShot

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -8,8 +8,15 @@ public class CoinController : MonoBehaviour {
     public Player player;
     public Text coinLabel;
 
+    AudioSource audioSource;
+
+    private void Start() {
+
+        audioSource = player.GetComponent<AudioSource>();
+    }
+
     public void GetCoin() {
-        player.GetComponent<AudioSource>().Play();
+        audioSource.PlayOneShot(audioSource.clip);
         player.coinCount++;
         coinLabel.text = "コイン×" + player.coinCount.ToString();
     }
