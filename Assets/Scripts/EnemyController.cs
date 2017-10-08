3c815b474ddac99667f266a85816bb4e0741e845
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-10-08 12:04:22 +0900

    敵を倒したときのUIを表示 #11

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class EnemyController : MonoBehaviour {
 
     public Text enemyCountLabel;
+    public GameObject panel;
+    [HideInInspector]
     public int enemyCount = 0;
     public StageController stageController;
     [HideInInspector]
@@ -14,19 +17,31 @@ public class EnemyController : MonoBehaviour {
     public PlayerInput playerInput;
 
     void Awake() {
+        panel.transform.DOMoveX(700, 1);
         enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
-        enemyCountLabel.text = "敵 × " + enemyCount.ToString();
+        enemyCountLabel.text = "あと" + enemyCount.ToString() + "体";
     }
 
     public void AttackEnemy() {
         enemyCount -= 1;
-        enemyCountLabel.text = "敵 × " + enemyCount.ToString();
+        if (enemyCount == 0) {
+            enemyCountLabel.text = "GOOD JOB!";
+        } else {
+            enemyCountLabel.text = "あと" + enemyCount.ToString() + "体";
+        }
+        panel.transform.DOMoveX(550, 1).SetEase(Ease.InOutSine);
+        Invoke("hide", 2);
+
     }
 
     public void gameOver() {
         isGameOver = true;
         stageController.gameOverPanel.transform.DOScale(new Vector3(1, 1, 1), 1);
         playerInput.canInput = false;
+    }
+
+    void hide() {
+        panel.transform.DOMoveX(700, 1).SetEase(Ease.InSine);
     }
 
 }
